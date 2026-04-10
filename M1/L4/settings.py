c93266555c917d66393cd8c1d85f7532f8f30306
Sánchez <sancrbna@gmail.com>
2026-04-09 19:36:02 -0500

    Create settings.py

---
+++ M1/L4/settings.py
@@ -0,0 +1,8 @@
+settings = {
+    "prefix": "$",
+    #El prefix sirve para identificar los comandos del bot;
+    #Es decir, es la palabra magica que el boy va a reconocer para ejecutar un comando
+    "TOKEN": "asdasdasdasd"
+    #El token es la contraseña del bot
+    #Osea lo que le permite conectarse a la API de discord y funcionar
+}
